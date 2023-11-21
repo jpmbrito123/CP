79f2bafc08f96f1a5eea489f622d0fa2a061259a
jpmbrito123 <joaomoreira75@gmail.com>
2023-11-21 22:30:48 +0000

    make a dar, script  com tudo

--- Fase2/code/run.sh
+++ Fase2/code/run.sh
@@ -5,8 +5,8 @@
 module load papi/5.4.1 
 #./MDseq.exe <inputdata.txt
 export OMP_NUM_THREADS=2
-./MDpar.exe < inputdata.txt 
+time ./MDpar.exe < inputdata.txt 
 export OMP_NUM_THREADS=4
-./MDpar.exe < inputdata.txt  
+time ./MDpar.exe < inputdata.txt  
 export OMP_NUM_THREADS=8
-./MDpar.exe < inputdata.txt   
+time ./MDpar.exe < inputdata.txt   
